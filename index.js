aa60393d191e27ce07b4f6b4d584ff20e30f1370
fajarfrdiansyah <fajarfirdiansyah21@gmail.com>
2022-01-26 16:57:40 +0700

    v3 setelah delete hour, menit, detik

--- index.js
+++ index.js
@@ -39,9 +39,6 @@ app.post('/tugasAkhir', (req, res) => {
         Distance: req.body.km,
         Average: req.body.avg,
         Calories: req.body.kalori,
-        Jam: req.body.hours,
-        Menit: req.body.minutes,
-        Detik: req.body.seconds,
         date: new Date()
     });
     res.send({
