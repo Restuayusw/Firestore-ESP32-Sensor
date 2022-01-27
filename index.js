9f0321f0695e293b5b1b0712ed5214244b6ca16d
fajarfrdiansyah <fajarfirdiansyah21@gmail.com>
2022-01-27 11:48:45 +0700

    change port to 10000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fire = require('./fire');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 6000
+const PORT = process.env.PORT || 10000
 
 app.use(cors());
 app.use(bodyParser.json());
